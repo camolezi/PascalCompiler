e98c93b59a39118893116c6cb49f376d58f52367
Daniel Eiji <danielchiyo@usp.br>
2020-04-21 20:58:39 -0300

    More reserved worlds added!

--- src/LexicalAnalyzer/ReservedWords.java
+++ src/LexicalAnalyzer/ReservedWords.java
@@ -17,13 +17,22 @@ class ReservedWords {
         //Table with all the language reserved words
         wordsMap = Map.ofEntries(
                 entry("program", "simb_program"),
-                entry("var", "simb_var"),
                 entry("begin", "simb_begin"),
-                entry("while", "simb_while"),
-                entry("integer", "simb_type_integer"),
                 entry("end", "simb_end"),
-                entry("do", "simb_do")
-
+                entry("var", "simb_var"),
+                entry("const","simb_const"),
+                entry("real","simb_type_real"),
+                entry("integer", "simb_type_integer"),
+                entry("procedure","simb_procedure"),
+                entry("else","simb_else"),
+                entry("read","simb_read"),
+                entry("write","simb_write"),
+                entry("while","simb_while"),
+                entry("do","simb_do"),
+                entry("if","simb_if"),
+                entry("then","simb_then"),
+                entry("for","simb_for"),
+                entry("to","simb_to")
         );
 
     }
